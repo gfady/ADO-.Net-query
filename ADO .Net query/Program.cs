be8e35a4558d54cac432c61fe3365857d51d8746
gfady <dashyk130903@gmail.com>
2023-12-12 15:21:02 +0300

    initial commit

---
+++ ADO .Net query/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+Console.WriteLine("Enter a user ID: ");
+string? userId = Console.ReadLine();
+
+string connectionString = "your_connection_string_here";
+using (SqlConnection connection = new(connectionString))
+{
+    connection.Open();
+
+    string query = "SELECT * FROM Users WHERE UserId = '" + userId + "'";
+    SqlCommand command = new(query, connection);
+
+    using SqlDataReader reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        Console.WriteLine($"User ID: {reader["UserId"]}, Username: {reader["Username"]}");
+    }
+}
